f820ea1170124af196295f378881d92918b930c6
vladislav202102 <vladikbar@mail.ru>
2021-02-27 10:55:20 +0300

    Main.java added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int weight = 70;
+        int percent = 100 * 100;
+        int height = 193 * 193;
+        int coefficient = weight * percent / height;
+        System.out.println(coefficient);
+    }
+}
