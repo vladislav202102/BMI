2a63b7fc7aad5fe9866830906f9eb92ce6b23b83
vladislav202102 <vladikbar@mail.ru>
2021-03-14 20:33:33 +0300

    fixed #1

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int weight = 70;
         int percent = 100 * 100;
-        int height = 193 * 193;
-        int coefficient = weight * percent / height;
+        float height = 193 * 193;
+        float coefficient = weight * percent / height;
         System.out.println(coefficient);
     }
 }
